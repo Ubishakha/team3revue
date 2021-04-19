200dd0cdd829335dbac4e054c126625109e49629
Bishakha <bupadhyaya@knox.edu>
2021-04-19 14:23:51 -0500

    Api Authorize Try 1

---
+++ client/src/services/Spotify.js
@@ -0,0 +1,11 @@
+// import axios from 'axios'
+import Api from '@/services/Api'
+// const request = axios.create({
+//   baseURL: "https://api.spotify.com/v1/"
+// });
+
+export default {
+  spotifyLogin(){
+    return Api().post("/authorize");
+  }
+}
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,28 +1,32 @@
 <template lang="html">
   <div class="home container">
-    <h1>Recent Posts</h1>
-    <PostPreview v-for="post in posts" :key="post.id" :post="post">
-      {{ post.title }}
-    </PostPreview>
-
-    <CreateButton></CreateButton>
-  </div>
+    <h1>Dashboard</h1>
+    
+    <CurrentTrack>
+    </CurrentTrack>
+     </div>
+  <input class="button connect" @click="connectToSpotify" type="submit" value="Connect To Spotify">
 </template>
 
 <script>
-import PostPreview from '@/components/PostPreview'
+// import { mapGetters } from "vuex";
+// import PostPreview from '@/components/PostPreview'
+import CurrentTrack from '@/components/CurrentTrack'
 import PostsService from '@/services/PostsService'
-import CreateButton from '@/components/CreateButton'
+import Spotify from '@/services/Spotify'
 
 export default {
   name: 'home',
 
-  components: { PostPreview, CreateButton },
+  components: { CurrentTrack },
 
-  data() {
-    return {
-      posts: null
-    }
+  methods: {
+        connectToSpotify() {
+          window.open(Spotify.spotifyLogin(), "_blank");
+        }
+      // connectToSpotify: function () {   
+      //     window.open("https://accounts.spotify.com/authorize", "_blank");    
+      // }
   },
 
   mounted() {
@@ -30,9 +34,14 @@ export default {
       .then(response => {
         this.posts = response.data
       })
+    
   }
+
 }
 </script>
 
-<style lang="css">
+<style scoped lang="css">
+.player{
+  display: block;
+}
 </style>
